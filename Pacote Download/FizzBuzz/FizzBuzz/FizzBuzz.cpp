bc92ce69831354e26f11d02fb11235ded5488549
isabelaleeite <61022844+isabelaleeite@users.noreply.github.com>
2020-09-29 12:01:46 -0300

    commit exercise

---
+++ Pacote Download/FizzBuzz/FizzBuzz/FizzBuzz.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdlib.h>
+#include<stdio.h>
+
+int main()
+{
+	for (int i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0) {
+			printf("FizzBuzz\n");
+		}
+		else if (i % 3 == 0) {
+			printf("Fizzz\n");
+		}
+		else if (i % 5 == 0) {
+			printf("Buzz\n");
+		}
+		else {
+			printf("%i\n", i);
+		}
+	}
+	
+}
